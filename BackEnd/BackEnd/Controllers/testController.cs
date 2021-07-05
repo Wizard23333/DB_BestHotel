91686139934019886b19cfdb200275d899c85a7b
tan-log <1585755131@qq.com>
2021-07-05 22:54:48 +0800

    重新整理后端框架，增加本地数据库连接

---
+++ BackEnd/BackEnd/Controllers/testController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BackEnd.Models;
+using BackEnd;
+namespace BackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class testController : ControllerBase
+    {
+        private readonly ILogger<testController> _logger;
+
+        public testController(ILogger<testController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public string Get()
+        {
+            return "HelloWorld";
+        }
+        [HttpPost]
+        public IEnumerable<User> Post()
+        {
+            DataAccess.CreateConn();
+            string ID = DataAccess.test();
+            DataAccess.CloseConn();
+            return Enumerable.Range(0,1).Select(index => new User
+            {
+                UserID=ID,
+            })
+            .ToArray();
+        }
+    }
+}
--- backEnd/backEnd/WeatherForecast.cs
+++ backEnd/backEnd/WeatherForecast.cs
@@ -1,6 +1,9 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace backEnd
+namespace BackEnd
 {
     public class WeatherForecast
     {
