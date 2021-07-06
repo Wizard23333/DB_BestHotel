bf51ae51fc893217b1a32d44fb1348c53c615df7
tan-log <1585755131@qq.com>
2021-07-06 20:10:57 +0800

    使用Swashbuckle.AspNetCore管理webapi

--- DB_BestHotel_BackEnd/BackEnd/Startup.cs
+++ DB_BestHotel_BackEnd/BackEnd/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace BackEnd
 {
@@ -26,6 +27,10 @@ namespace BackEnd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +44,15 @@ namespace BackEnd
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            //启用中间件服务生成Swagger作为JSON终结点
+            app.UseSwagger();
+            //启用中间件服务对swagger-ui，指定Swagger JSON终结点
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseAuthorization();
 
