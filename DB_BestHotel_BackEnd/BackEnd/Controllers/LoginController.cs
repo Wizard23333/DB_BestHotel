55a6499036cf8e1fbc30218cf2cbc7fd4911bc7c
tan-log <1585755131@qq.com>
2021-07-13 03:37:33 +0800

    接口实现
    登陆接口、注册接口、密保接口、修改密码接口、客户信息增删查改接口

--- DB_BestHotel_BackEnd/BackEnd/Controllers/LoginController.cs
+++ DB_BestHotel_BackEnd/BackEnd/Controllers/LoginController.cs
@@ -9,7 +9,6 @@ using BackEnd;
 namespace BackEnd.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
     public class LoginController : ControllerBase
     {
         private readonly ILogger<LoginController> _logger;
@@ -19,6 +18,7 @@ namespace BackEnd.Controllers
             _logger = logger;
         }
         [HttpPost]
+        [Route("api/[controller]")]
         [ApiResponseFilterAttribute]
         public IdentityResponse Post([FromBody] IdentityRequest value)
         {
@@ -26,11 +26,81 @@ namespace BackEnd.Controllers
             {
                 if (DataAccess.IsUserExist(value.user_id, value.user_password) == true)
                 {
-                    var date = DataAccess.FindUserInfo(value.user_id);
-                    return new IdentityResponse { user_id = date.user_id, user_name = date.user_name, user_type = date.user_type };
+                    var data = DataAccess.FindUserInfo(value.user_id);
+                    return new IdentityResponse { user_id = data.user_id, user_name = data.user_name, user_type = data.user_type };
                 }
             }
             catch(Exception ex){
+                Console.WriteLine(ex);
+            }
+            return null;
+        }
+
+        [HttpPost]
+        [Route("api/[controller]/Register/Client")]
+        [ApiResponseFilterAttribute]
+        public StatusCodeResult Post([FromBody] RegClientRequest value)
+        {
+                try
+                {
+                    User data1 = new User { user_id = value.client_id, user_name = value.client_name, user_password = value.password, user_type = "1", security_q = value.security_q, s_q_answer = value.s_q_answer };
+                    Client data2 = new Client { client_id = value.client_id, client_name = value.client_name, client_sex = value.client_sex, client_birthday = value.client_birthday, client_mobile = value.client_mobile, client_idCard = value.client_idCard };
+                    if (DataAccess.AddUser(data1)&& DataAccess.AddClient(data2))
+                        return new StatusCodeResult(200);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                return null;
+        }
+        [HttpPost]
+        [Route("api/[controller]/Register/Staff")]
+        [ApiResponseFilterAttribute]
+        public StatusCodeResult Post([FromBody] RegStaffRequest value)
+        {
+            try
+            {
+                User data1 = new User { user_id = value.staff_id, user_name = value.staff_name, user_password = value.password, user_type = "2", security_q = value.security_q, s_q_answer = value.s_q_answer };
+                Staff data2 =value;
+                if (DataAccess.AddUser(data1) && DataAccess.AddStaff(data2))
+                    return new StatusCodeResult(200);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return null;
+        }
+        [HttpPost]
+        [Route("api/[controller]/Forget/{id}")]
+        [ApiResponseFilterAttribute]
+        public StatusCodeResult Post(string id,[FromBody] ForgetRequest value)
+        {
+            try
+            {
+                var data = DataAccess.FindUserInfo(id);
+                if ((data.s_q_answer == value.s_q_answer)&&(DataAccess.AlterUserPassword(id,value.user_password)==true))
+                 return new StatusCodeResult(200);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return new StatusCodeResult(501);
+        }
+        [HttpGet]
+        [Route("api/[controller]/Forget/{id}")]
+        [ApiResponseFilterAttribute]
+        public ForgetResponse Get(string id)
+        {
+            try
+            {
+                var data = DataAccess.FindUserInfo(id);
+                return new ForgetResponse { user_id=id,security_q=data.security_q };
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine(ex);
             }
             return null;
--- DB_BestHotel_BackEnd/BackEnd/Models/DataAccess.cs
+++ DB_BestHotel_BackEnd/BackEnd/Models/DataAccess.cs
@@ -61,20 +61,77 @@ namespace BackEnd.Models
             }
             return a;
         }
+
+        
+
         //向Users表中增加一个新用户(注册)
-        //添加成功返回user_id，添加失败返回“0”
-        public static string AddUser(string user_id, string UserPassword)
+        //添加成功返回true，添加失败返回“0”
+        public static bool AddUser(User req)
         {
+            CreateConn();
             OracleCommand Insert = DB.CreateCommand();
-            Insert.CommandText = "insert into USERS values(:user_id,:UserPassword)";
-            Insert.Parameters.Add(new OracleParameter(":user_id", user_id));
-            Insert.Parameters.Add(new OracleParameter(":UserPassword", UserPassword));
+            Insert.CommandText = "insert into USERS values(:USER_ID,:USER_PASSWORD,:USER_TYPE,:SECURITY_Q,:S_Q_ANSWER,:USER_NAME)";
+            Insert.Parameters.Add(new OracleParameter(":USER_ID", req.user_id));
+            Insert.Parameters.Add(new OracleParameter(":USER_PASSWORD", req.user_password));
+            Insert.Parameters.Add(new OracleParameter(":USER_TYPE", req.user_type));
+            Insert.Parameters.Add(new OracleParameter(":SECURITY_Q", req.security_q));
+            Insert.Parameters.Add(new OracleParameter(":S_Q_ANSWER", req.s_q_answer));
+            Insert.Parameters.Add(new OracleParameter(":USER_NAME", req.user_name));
             int Result = Insert.ExecuteNonQuery();
+            CloseConn();
             if (Result == 1)
             {
-                return user_id;
+                return true;
             }
-            else return "0";
+            else return false;
+        }
+
+        //向Client表中增加一个新客户(注册)
+        //添加成功返回1，添加失败返回0
+        public static bool AddClient(Client req)
+        {
+            CreateConn();
+            OracleCommand Insert = DB.CreateCommand();
+            Insert.CommandText = "insert into CLIENT values(:CLIENT_ID,:CLIENT_NAME,:CLIENT_SEX,to_date(:CLIENT_BIRTHDAY,'YYYY-MM-DD'),:CLIENT_TELEPHONENUMBER,:CLIENT_IDENTITY_CARD_NUMBER)";
+            Insert.Parameters.Add(new OracleParameter(":CLIENT_ID", req.client_id));
+            Insert.Parameters.Add(new OracleParameter(":CLIENT_NAME", req.client_name));
+            Insert.Parameters.Add(new OracleParameter(":CLIENT_SEX", req.client_sex));
+            Insert.Parameters.Add(new OracleParameter(":CLIENT_BIRTHDAY", req.client_birthday));
+            Insert.Parameters.Add(new OracleParameter(":CLIENT_TELEPHONENUMBER", req.client_mobile));
+            Insert.Parameters.Add(new OracleParameter(":CLIENT_IDENTITY_CARD_NUMBER", req.client_idCard));
+            int Result = Insert.ExecuteNonQuery();
+            CloseConn();
+            if (Result == 1)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        //向Staff表中增加一个新客户(注册)
+        //添加成功返回true，添加失败返回false
+        public static bool AddStaff(Staff req)
+        {
+            CreateConn();
+            OracleCommand Insert = DB.CreateCommand();
+            Insert.CommandText = "insert into STAFF values(	:STAFF_ID,:STAFF_NAME,:STAFF_SEX,:STAFF_AGE	,:STAFF_IDENTITY_CARD_NUMBER,:STAFF_ADDRESS	,:STAFF_DEPARTMENT,:STAFF_POSITION,to_date(:STAFF_ENTRY_DATE,'YYYY-MM-DD'),:STAFF_SALARY)";
+            Insert.Parameters.Add(new OracleParameter("	:STAFF_ID	", req.staff_id));
+            Insert.Parameters.Add(new OracleParameter("	:STAFF_NAME	", req.staff_name));
+            Insert.Parameters.Add(new OracleParameter("	:STAFF_SEX	", req.staff_sex));
+            Insert.Parameters.Add(new OracleParameter("	:STAFF_AGE	", req.staff_age));
+            Insert.Parameters.Add(new OracleParameter("	:STAFF_IDENTITY_CARD_NUMBER	", req.staff_identity_card_number));
+            Insert.Parameters.Add(new OracleParameter("	:STAFF_ADDRESS	", req.staff_address));
+            Insert.Parameters.Add(new OracleParameter("	:STAFF_DEPARTMENT	", req.staff_department));
+            Insert.Parameters.Add(new OracleParameter("	:STAFF_POSITION	", req.staff_position));
+            Insert.Parameters.Add(new OracleParameter("	:STAFF_ENTRY_DATE	", req.staff_entry_date));
+            Insert.Parameters.Add(new OracleParameter("	:STAFF_SALARY	", req.staff_salary));
+            int Result = Insert.ExecuteNonQuery();
+            CloseConn();
+            if (Result == 1)
+            {
+                return true;
+            }
+            else return false;
         }
 
         //查找个人信息
@@ -89,26 +146,79 @@ namespace BackEnd.Models
             while (Ord.Read())
             {
                 a.user_id = Ord.GetValue(0).ToString();
-                a.user_name = Ord.GetValue(1).ToString();
-                a.user_password = Ord.GetValue(2).ToString();
-                a.user_type = (Ord.GetValue(3).ToString() == "1" ? "client":"staff");
+                a.user_name = Ord.GetValue(5).ToString();
+                a.user_password = Ord.GetValue(1).ToString();
+                a.user_type = (Ord.GetValue(2).ToString() == "1" ? "client":"staff");
+                a.security_q = Ord.GetValue(3).ToString();
+                a.s_q_answer = Ord.GetValue(4).ToString();
             }
             CloseConn();
             return a;
         }
-
-
-        public static List<Order> DisplayOrderInfo(string query,out int total)
+        //查找客户信息
+        public static List<Client> FindClientInfo()
         {
-            List<Order> orders = new List<Order>();
+            CreateConn();
             OracleCommand Search = DB.CreateCommand();
-            Search.CommandText = "select " + query + "from room_order";
+            Search.CommandText = "select * from CLIENT";
             OracleDataReader Ord = Search.ExecuteReader();
+            List<Client> a = new List<Client>();
             while (Ord.Read())
             {
-                orders.Add(new Order { order_id = Ord.GetValue(0).ToString(), client_id = Ord.GetValue(1).ToString(), order_date = Ord.GetValue(3).ToString(), amount = (int)Ord.GetValue(4), state = (int)Ord.GetValue(5) });
+                a.Add(new Client
+                {
+                    client_id = Ord.GetValue(0).ToString() ,  client_name = Ord.GetValue(1).ToString() ,  client_sex = Ord.GetValue(2).ToString() ,  client_birthday = Ord.GetValue(3).ToString() ,  client_mobile = Ord.GetValue(4).ToString()  , client_idCard = Ord.GetValue(5).ToString()
+                });
             }
-            return orders;
+            return a;
+        }
+        public static Client FindClientInfo(string client_id)
+        {
+            CreateConn();
+            OracleCommand Search = DB.CreateCommand();
+            Search.CommandText = "select * from CLIENT where CLIENT_ID=:client_id";
+            Search.Parameters.Add(new OracleParameter(":client_id", client_id));
+            OracleDataReader Ord = Search.ExecuteReader();
+            Client a = new Client();
+            while (Ord.Read())
+            {
+                a.client_id = Ord.GetValue(0).ToString();
+                a.client_name = Ord.GetValue(1).ToString();
+                a.client_sex = Ord.GetValue(2).ToString();
+                a.client_birthday = Ord.GetValue(3).ToString();
+                a.client_mobile = Ord.GetValue(4).ToString();
+                a.client_idCard = Ord.GetValue(5).ToString();
+            }
+            return a;
+        }
+        public static bool AlterUserPassword(string id, string password)
+        {
+            CreateConn();
+            OracleCommand CMD = DB.CreateCommand();
+            CMD.CommandText = "update USERS set USER_PASSWORD=:password where USER_ID=:id";
+            CMD.Parameters.Add(new OracleParameter(":password", password));
+            CMD.Parameters.Add(new OracleParameter(":id", id));
+            int Result = CMD.ExecuteNonQuery();
+            CloseConn();
+            if (Result != 1)
+                return false;
+            else
+                return true;
+        }
+        public static bool AlterClient(string id,string phone)
+        {
+            CreateConn();
+            OracleCommand CMD = DB.CreateCommand();
+            CMD.CommandText = "update CLIENT set CLIENT_TELEPHONENUMBER=:phone where CLIENT_ID=:id";
+            CMD.Parameters.Add(new OracleParameter(":phone", phone));
+            CMD.Parameters.Add(new OracleParameter(":id", id));
+            int Result = CMD.ExecuteNonQuery();
+            CloseConn();
+            if (Result != 1)
+                return false;
+            else
+                return true;
+
         }
     }
 }
