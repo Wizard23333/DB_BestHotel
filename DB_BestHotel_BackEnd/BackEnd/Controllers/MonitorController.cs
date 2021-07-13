12db1917d23e739a130fb35f83815ed809e006a8
tan-log <1585755131@qq.com>
2021-07-13 12:03:59 +0800

    房间设施接口

---
+++ DB_BestHotel_BackEnd/BackEnd/Controllers/MonitorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BackEnd.Models;
+using BackEnd;
+namespace BackEnd.Controllers
+{
+    [ApiController]
+    public class MonitorController : ControllerBase
+    {
+        private readonly ILogger<MonitorController> _logger;
+
+        public MonitorController(ILogger<MonitorController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        [Route("api/[controller]")]
+        [ApiResponseFilterAttribute]
+        public List<MonitorRequest> Get()
+        {
+            try
+            {
+                var data = DataAccess.FindMonitorInfo();
+                return data;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return null;
+        }
+        
+    }
+}
